65e67835356b36828f52b0198306adc115cf09c2
gvelsamy <gnanam.java@gmail.com>
2025-05-29 17:20:40 +0200

    kafka consumer service change

--- src/main/java/com/jarvis/springbootkafka/service/KafkaConsumerService.java
+++ src/main/java/com/jarvis/springbootkafka/service/KafkaConsumerService.java
@@ -7,9 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
 
+    private String message;
+
     @KafkaListener(topics = "my-topic", groupId = "my-group")
     public void consumeMessage(ConsumerRecord<String, String> record){
+        message = record.value();
         System.out.println("Received message: "+ record.value());
+    }
+
+    public String getMessage() {
+        return message;
     }
 
 }
