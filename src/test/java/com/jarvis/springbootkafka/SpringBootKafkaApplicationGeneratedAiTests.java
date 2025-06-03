47e986a0afe9fa01801979e0dfd6856563cd7f10
gvelsamy <gnanam.java@gmail.com>
2025-06-03 14:57:29 +0200

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/com/jarvis/springbootkafka/SpringBootKafkaApplicationGeneratedAiTests.java
@@ -0,0 +1,24 @@
+package com.jarvis.springbootkafka;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions; 
+
+public class SpringBootKafkaApplicationGeneratedAiTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testMain() {
+        // GIVEN: 
+        String[] args = new String[0];
+
+        // WHEN: 
+        SpringApplication.run(SpringBootKafkaApplication.class, args);
+
+        // THEN: 
+        Assertions.assertTrue(true); // Add assertion based on expected behavior
+    }
+}
---
+++ src/test/java/com/jarvis/springbootkafka/service/KafkaConsumerServiceGeneratedAiTests.java
@@ -0,0 +1,3 @@
+package com.jarvis.springbootkafka.service;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord; 
