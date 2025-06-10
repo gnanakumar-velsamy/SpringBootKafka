6e99af39a584e00df2bf1c755a30e4b35e091076
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-06-10 16:54:41 +0200

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/com/jarvis/springbootkafka/service/KafkaConsumerServiceGeneratedAiTests.java
@@ -0,0 +1,25 @@
+package com.jarvis.springbootkafka.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class KafkaConsumerServiceGeneratedAiTests {
+
+    private KafkaConsumerService kafkaConsumerService;
+
+    @BeforeEach
+    public void setUp() {
+        kafkaConsumerService = new KafkaConsumerService();
+    }
+
+    @Test
+    public void testConsumeMessage() {
+        // GIVEN a message to be consumed
+        String message = "Hello from Kafka!";
+        // WHEN the message is consumed
+        kafkaConsumerService.consumeMessage(message);
+        // THEN the message should be stored in the service instance
+        assertEquals(message, kafkaConsumerService.getMessage());
+    }
+
+}
