7fa4982d259399d591c56075826abda4c762da90
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-06-12 13:02:05 +0200

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/com/jarvis/springbootkafka/SpringBootKafkaApplicationGeneratedAiTests.java
@@ -0,0 +1,22 @@
+package com.jarvis.springbootkafka;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class SpringBootKafkaApplicationGeneratedAiTests {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void testMain() {
+		// GIVEN - Context setup
+		String[] args = new String[0];
+		// WHEN - Action to be tested
+		SpringApplication.run(SpringBootKafkaApplication.class, args);
+		// THEN - Expected outcome
+		Assertions.assertTrue(true); 
+	}
+}
