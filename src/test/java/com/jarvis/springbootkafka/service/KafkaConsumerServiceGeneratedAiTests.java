f52ee9471cf4042e9fbc58005a056127a1fd92dd
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-07-21 10:54:16 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/jarvis/springbootkafka/service/KafkaConsumerServiceGeneratedAiTests.java
@@ -0,0 +1,25 @@
+package com.jarvis.springbootkafka.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KafkaConsumerServiceGeneratedAiTests {
+
+    private KafkaConsumerService kafkaConsumerService;
+
+    @BeforeEach
+    void setUp() {
+        kafkaConsumerService = new KafkaConsumerService();
+    }
+
+    @Test
+    void testConsumeMessage() {
+        // GIVEN a message to consume
+        String expectedMessage = "Hello from Kafka!";
+        // WHEN the message is consumed
+        kafkaConsumerService.consumeMessage(expectedMessage);
+        // THEN the message should be stored in the service
+        assertEquals(expectedMessage, kafkaConsumerService.getMessage());
+    }
+}
