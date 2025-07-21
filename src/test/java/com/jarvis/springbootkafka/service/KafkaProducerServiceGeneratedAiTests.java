2f29bd619a4124139feac51be79af9ac7f058084
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-07-21 19:06:32 +0200

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/com/jarvis/springbootkafka/service/KafkaProducerServiceGeneratedAiTests.java
@@ -0,0 +1,8 @@
+package com.jarvis.springbootkafka.service;
+
+```xml
+<dependency>
+    <groupId>org.springframework.kafka</groupId>
+    <artifactId>spring-kafka</artifactId>
+    <version>3.0.0-RELEASE</version> </dependency> 
+```
