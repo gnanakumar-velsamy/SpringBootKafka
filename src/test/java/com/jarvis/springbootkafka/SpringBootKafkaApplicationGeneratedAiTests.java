bf07b9bc0233306f467c5b37085a5216e0901a1e
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-07-22 19:38:27 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/jarvis/springbootkafka/SpringBootKafkaApplicationGeneratedAiTests.java
@@ -0,0 +1,18 @@
+package com.jarvis.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpringBootKafkaApplicationGeneratedAiTests {
+
+    @Test
+    void testMain() {
+        // GIVEN: A String array of command-line arguments
+        String[] args = {}; 
+        // WHEN: The main method is executed
+        SpringApplication.run(SpringBootKafkaApplication.class, args);
+        // THEN: An exception should not be thrown
+    }
+
+}
