e09965d3f6a59458caba706919582deee873220c
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-07-25 12:39:57 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/jarvis/springbootkafka/SpringBootKafkaApplicationGeneratedAiTests.java
@@ -0,0 +1,16 @@
+package com.jarvis.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationGeneratedAiTests {
+
+    @Test
+    void testMainMethodStartsApplication() {
+        // GIVEN - The application context is set up for testing
+        // WHEN - The main method of the application is executed
+        SpringBootKafkaApplication.main(new String[]{});
+        // THEN - Assert that a specific log message indicating successful startup is present in the logs
+    }
+}
