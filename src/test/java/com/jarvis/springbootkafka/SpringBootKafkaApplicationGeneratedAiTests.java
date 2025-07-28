0ce7e61cf2ab7606e5a4d3f856f54e8876783d14
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-07-28 15:38:02 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/jarvis/springbootkafka/SpringBootKafkaApplicationGeneratedAiTests.java
@@ -0,0 +1,15 @@
+package com.jarvis.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest
+class SpringBootKafkaApplicationGeneratedAiTests {
+
+    @Test
+    void testMain() {
+        // GIVEN - Context setup is not applicable for main method
+        // THEN -  The application should start successfully
+    }
+}
