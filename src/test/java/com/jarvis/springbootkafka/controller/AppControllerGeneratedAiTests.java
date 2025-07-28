26660880744d6a3e057622f7630d83499d44a8fc
gvelsamy <gvelsamy@LPL-5CG1075MBG.corp.capgemini.com>
2025-07-28 14:59:06 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/jarvis/springbootkafka/controller/AppControllerGeneratedAiTests.java
@@ -0,0 +1,32 @@
+package com.jarvis.springbootkafka.controller;
+
+import com.jarvis.springbootkafka.service.KafkaProducerService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+public class AppControllerGeneratedAiTests {
+    @Mock
+    private KafkaProducerService kafkaProducerService;
+
+    @InjectMocks
+    private AppController appController;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void sendMessage_shouldCallKafkaProducerServiceSendMethod() {
+        // GIVEN - A message to be sent
+        String message = "test message";
+        // WHEN - The sendMessage method is called
+        appController.sendMessage(message);
+        // THEN - Verify that the kafkaProducerService.send method was called with the provided message
+        verify(kafkaProducerService).send(message);
+    }
+}
