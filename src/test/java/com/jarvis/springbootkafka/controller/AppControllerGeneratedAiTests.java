7650363842e67f03eb581a82a71078e7161549be
gvelsamy <gnanam.java@gmail.com>
2025-07-31 15:55:49 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/jarvis/springbootkafka/controller/AppControllerGeneratedAiTests.java
@@ -0,0 +1,36 @@
+package com.jarvis.springbootkafka.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.jarvis.springbootkafka.service.KafkaProducerService;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AppController.class)
+public class AppControllerGeneratedAiTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private KafkaProducerService kafkaProducerService;
+
+    @Test
+    void sendMessage_shouldCallKafkaProducerService() throws Exception {
+        // GIVEN - A message to send
+        String message = "test message";
+        // WHEN - Send the message
+        mockMvc.perform(post("/send")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(message))
+                .andExpect(status().isOk());
+        // THEN - Verify that KafkaProducerService was called with the message
+        verify(kafkaProducerService).send(message);
+    }
+}
