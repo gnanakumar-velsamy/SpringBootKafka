d2246186688f1899081a70fb637983fcfc39b433
gvelsamy <gnanam.java@gmail.com>
2025-07-31 17:03:24 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/jarvis/springbootkafka/SpringBootKafkaApplicationGeneratedAiTests.java
@@ -0,0 +1,17 @@
+package com.jarvis.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationGeneratedAiTests {
+
+    @Test
+    void testMainMethodRunsSuccessfully() {
+        // GIVEN - The application context is set up by Spring Boot Test
+        // WHEN -  The main method of the application is executed
+        SpringBootKafkaApplication.main(new String[] {}); 
+        // THEN - The application should start successfully without throwing any exceptions
+        assert true; 
+    }
+}
