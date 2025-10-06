64a2691afe973c854f341b4332c9fd936ae7b13c
gvelsamy <gnanam.java@gmail.com>
2025-10-06 14:06:19 +0200

    [codeai] Generate unresolved_build_errors classes

---
+++ src/test/java/com/jarvis/springbootkafka/controller/AppControllerGeneratedAiTests.java
@@ -0,0 +1,30 @@
+package com.jarvis.springbootkafka.controller;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+class AppControllerGeneratedAiTests {
+    @InjectMocks
+    private AppController appController;
+    @Mock
+    private KafkaProducerService kafkaProducerService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void sendMessage_shouldCallKafkaProducerServiceSendMethod() {
+        // GIVEN - A message to be sent
+        String message = "test message";
+        // WHEN - The sendMessage method is called
+        appController.sendMessage(message);
+        // THEN - verify that the send method of KafkaProducerService was called with the provided message
+        verify(kafkaProducerService).send(message);
+    }
+}
